ce624f4a8f430cc34b9ec23a249f8bea406743e4
Krishnan Kannan <kxkannan@gmail.com>
2017-11-11 10:49:14 -0700

    style: fixed indendation, formatting.

--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
@@ -17,47 +17,52 @@ class BookList extends Component {
   }
 
   render() {
-    const { books, onShelfChange } = this.props
-    const { shelf } = this.props
-    const { query } = this.state
+    const {books, onShelfChange} = this.props
+    const {shelf} = this.props
+    const {query} = this.state
 
     let booksOnShelf
     if (shelf && shelf != "searchResults") {
-      booksOnShelf = books.filter((book) => book.shelf == shelf )
+      booksOnShelf = books.filter((book) => book.shelf == shelf)
     } else {
       booksOnShelf = books
     }
 
-    return(
-      <div className="bookshelf-books">
+    return (
+        <div className="bookshelf-books">
 
 
-      <ol className="books-grid">
-      {booksOnShelf.map ( (book) => (
-        <li key={book.id} >
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + book.backgroundImage + ")" }}></div>
-              <div className="book-shelf-changer">
-                <select onChange={(e) => onShelfChange(book.id, book.shelf, e)} value={shelf}>
-                  <option value="none" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.author}</div>
-          </div>
-        </li>
-        )) }
-      </ol>
-      </div>
+          <ol className="books-grid">
+            {booksOnShelf.map((book) => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: "url(" + book.backgroundImage + ")"
+                      }}></div>
+                      <div className="book-shelf-changer">
+                        <select onChange={(e) => onShelfChange(book.id, book.shelf, e)} value={shelf}>
+                          <option value="none" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.author}</div>
+                  </div>
+                </li>
+            ))}
+          </ol>
+        </div>
     )
-   }
- }
+  }
+}
+
 BookList.propTypes = {
   onShelfChange: PropTypes.func.isRequired,
   books: PropTypes.array.isRequired,
